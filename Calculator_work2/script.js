a0787e9cf85daebc3f3e8a655a79e0778706a0f0
Orel <orelush555@gmail.com>
2023-05-22 18:56:57 +0300

    added check numeric number to input text

--- Calculator_work2/script.js
+++ Calculator_work2/script.js
@@ -1,8 +1,14 @@
 function sumbit() {
-    const x = parseInt(document.getElementById("num_x").value);
-    const y = parseInt(document.getElementById("num_y").value);
-    const sum = x + y;
-    const product = x * y;
+  const x = document.getElementById("num_x").value;
+  const y = document.getElementById("num_y").value;
+  const numericPattern = /^\d+$/;
+  if (!numericPattern.test(x) || !numericPattern.test(y)) {
+    alert("Please enter only numeric values!");
+  } else {
+    const sum = parseInt(x) + parseInt(y);
+    const product = parseInt(x) * parseInt(y);
+    
     document.getElementById("operator1").value = sum;
     document.getElementById("operator2").value = product;
   }
+}
